3d556a0061819bd5f72393080eb0d59adcb05922
SleepyAltair <gergana.blagoeva02@gmail.com>
2024-02-11 14:20:26 +0200

    Add levels data

---
+++ levels/tilesets/card_front_river.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="card_front_river" tilewidth="100" tileheight="148" tilecount="1" columns="1">
+ <image source="../../graphics/card_front_river.png" width="100" height="148"/>
+</tileset>
